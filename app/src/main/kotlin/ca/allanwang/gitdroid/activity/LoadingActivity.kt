75ddb39502679e77bdd31d8563ee31d07f985ada
Allan Wang <me@allanwang.ca>
2019-06-20 12:13:16 -0700

    Split bsae activity to loading activity

---
+++ app/src/main/kotlin/ca/allanwang/gitdroid/activity/LoadingActivity.kt
@@ -0,0 +1,66 @@
+package ca.allanwang.gitdroid.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
+import ca.allanwang.gitdroid.ktx.utils.L
+import kotlin.reflect.KProperty
+
+abstract class LoadingActivity<Binding : ViewDataBinding> : BaseActivity() {
+
+
+    lateinit var binding: Binding
+
+    abstract val layoutRes: Int
+        @LayoutRes get
+
+    private val requiredExtras: MutableList<String> = mutableListOf()
+
+    override fun setIntent(newIntent: Intent?) {
+        missingExtras(intent)
+        super.setIntent(newIntent)
+    }
+
+    protected fun stringExtra(key: String) = intentDelegate(key) { getStringExtra(key)!! }
+    protected fun intExtra(key: String) = intentDelegate(key) { getIntExtra(key, 0) }
+
+    private fun <T> intentDelegate(key: String, getter: Intent.() -> T): IntentDelegate<T> {
+        requiredExtras.add(key)
+        return IntentDelegate(getter)
+    }
+
+    protected inner class IntentDelegate<T>(val getter: Intent.() -> T) {
+        operator fun getValue(thisRef: Any?, property: KProperty<*>): T = intent.getter()
+    }
+
+    private fun missingExtras(intent: Intent?): Boolean {
+        if (requiredExtras.isEmpty()) {
+            return false
+        }
+        if (intent == null) {
+            L.fail { "Required extras $requiredExtras, but intent is null" }
+            finish()
+            return true
+        }
+        val missing = requiredExtras.filter { intent.hasExtra(it) }
+        if (missing.isEmpty()) {
+            return false
+        }
+        L.fail { "Missing extras $missing" }
+        finish()
+        return true
+    }
+
+    final override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (missingExtras(intent)) {
+            return
+        }
+        binding = bindContentView(layoutRes)
+        onCreate2(savedInstanceState)
+    }
+
+    abstract fun onCreate2(savedInstanceState: Bundle?)
+
+}
--- app/src/main/kotlin/ca/allanwang/gitdroid/activity/RepoActivity.kt
+++ app/src/main/kotlin/ca/allanwang/gitdroid/activity/RepoActivity.kt
@@ -1,13 +1,48 @@
 package ca.allanwang.gitdroid.activity
 
+import android.content.Context
 import android.os.Bundle
 import ca.allanwang.gitdroid.R
+import ca.allanwang.gitdroid.views.Adapter
 import ca.allanwang.gitdroid.views.databinding.ViewRepoFilesBinding
+import ca.allanwang.kau.utils.startActivity
+import github.fragment.FullRepo
+import kotlinx.coroutines.launch
 
-class RepoActivity : BaseActivity() {
+class RepoActivity : LoadingActivity<ViewRepoFilesBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding = bindContentView<ViewRepoFilesBinding>(R.layout.view_repo_files)
+    override val layoutRes: Int = R.layout.view_repo_files
+
+    val query by stringExtra(ARG_QUERY)
+
+    lateinit var treeAdapter: Adapter
+
+    override fun onCreate2(savedInstanceState: Bundle?) {
+        treeAdapter = Adapter.bind(binding.repoRecycler)
+        launch {
+            val repo = gdd.getRepo(query).await()
+            val defaultBranch = repo.defaultBranchRef
+            if (defaultBranch == null) {
+
+            } else {
+                val entries = defaultBranch
+                    .target
+                    .let { it as FullRepo.AsCommit }
+                    .tree
+                    .entries
+                    ?.map { it.fragments.treeEntryItem }?: emptyList()
+            }
+
+        }
+    }
+
+    companion object {
+        private const val ARG_QUERY = "arg_query"
+
+        fun launch(context: Context, nameWithOwner: String) {
+            context.startActivity<RepoActivity>(intentBuilder = {
+                putExtra(ARG_QUERY, nameWithOwner)
+            })
+        }
     }
 }
