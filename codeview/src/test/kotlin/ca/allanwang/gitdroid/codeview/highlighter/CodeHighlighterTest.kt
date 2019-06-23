51db37f98c6e4037155c193974ba558c6bba1cd5
Allan Wang <me@allanwang.ca>
2019-06-23 12:10:42 -0700

    Add back jansi support

--- codeview/src/test/kotlin/ca/allanwang/gitdroid/codeview/highlighter/CodeHighlighterTest.kt
+++ codeview/src/test/kotlin/ca/allanwang/gitdroid/codeview/highlighter/CodeHighlighterTest.kt
@@ -7,62 +7,81 @@ import ca.allanwang.gitdroid.codeview.pattern.resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.fusesource.jansi.Ansi
 import org.junit.Test
 
+private fun Ansi.italic() = a(Ansi.Attribute.ITALIC)
+private fun Ansi.underline() = a(Ansi.Attribute.UNDERLINE)
+// Ansi.ansi() from library checks for enabled flag; we won't
+private fun ansi(): Ansi = Ansi()
+
 class CodeHighlighterTest {
+
+    class AnsiAppendable : Appendable {
+
+        val ansi = ansi()
+
+        override fun append(csq: CharSequence?): Appendable {
+            ansi.a(csq)
+            return this
+        }
+
+        override fun append(csq: CharSequence?, start: Int, end: Int): Appendable {
+            ansi.a(csq, start, end)
+            return this
+        }
+
+        override fun append(c: Char): Appendable {
+            ansi.a(c)
+            return this
+        }
+
+        override fun toString(): String = ansi.toString()
+
+        override fun hashCode(): Int = ansi.hashCode()
+
+        override fun equals(other: Any?): Boolean = ansi == (other as? AnsiAppendable)?.ansi
+    }
 
     /**
      * See http://ascii-table.com/ansi-escape-sequences.php
      */
-    object AnsiHighlightBuilder : CodeHighlightBuilder<StringBuilder, String> {
-
-        const val ESC = "\u001B["
-        const val RESET = "${ESC}0m"
-        const val BOLD = "${ESC}1m"
-        const val ITALIC = "${ESC}3m"
-        const val UNDERLINE = "${ESC}4m"
-        const val BLACK = "${ESC}30m"
-        const val RED = "${ESC}31m"
-        const val GREEN = "${ESC}32m"
-        const val YELLOW = "${ESC}33m"
-        const val BLUE = "${ESC}34m"
-        const val MAGENTA = "${ESC}35m"
-        const val CYAN = "${ESC}36m"
-        const val WHITE = "${ESC}37m"
+    object AnsiHighlightBuilder : CodeHighlightBuilder<AnsiAppendable, String> {
 
         override fun create(pr: PR, text: String): String {
-            val flag: String = when (pr) {
-                PR.AttrName -> "$RED$BOLD"
-                PR.AttrValue -> RED
-                PR.Comment -> "$GREEN$ITALIC"
-                PR.Declaration -> "$CYAN$BOLD"
-                PR.Keyword -> "$BLACK$BOLD"
-                PR.Literal -> BLUE
-                PR.Nocode -> YELLOW
-                PR.Plain -> RESET
-                PR.Punctuation -> MAGENTA
-                PR.Source -> "$MAGENTA$BOLD"
-                PR.String -> "$BLUE$BOLD"
-                PR.Tag -> "$BLUE$UNDERLINE"
-                PR.Type -> CYAN
-            }
-            return "$flag$text$RESET"
+            return ansi().run {
+                when (pr) {
+                    PR.AttrName -> fgRed().bold()
+                    PR.AttrValue -> fgRed()
+                    PR.Comment -> fgGreen().italic()
+                    PR.Declaration -> fgCyan().bold()
+                    PR.Keyword -> fgBlack().bold()
+                    PR.Literal -> fgBlue()
+                    PR.Nocode -> fgYellow()
+                    PR.Plain -> this
+                    PR.Punctuation -> fgMagenta()
+                    PR.Source -> fgMagenta().bold()
+                    PR.String -> fgBlue().bold()
+                    PR.Tag -> fgBlue().underline()
+                    PR.Type -> fgCyan()
+                }
+            }.a(text).toString()
         }
 
-        override fun createBuilder(): StringBuilder = StringBuilder()
+        override fun createBuilder(): AnsiAppendable = AnsiAppendable()
 
-        override fun build(builder: StringBuilder): String = builder.toString()
+        override fun build(builder: AnsiAppendable): String = builder.toString()
 
     }
 
     @Test
     fun ansiPreview() {
-        with(AnsiHighlightBuilder) {
-            println("Normal text")
-            println("$BLACK${BOLD}Black and bold")
-            println("$WHITE${ITALIC}White and italic")
-            println("$BLUE${UNDERLINE}Blue and underline")
-        }
+        println(
+            ansi().a("Normal text").newline()
+                .fgBlack().bold().a("Black and bold").newline()
+                .fgCyan().italic().a("Cyan and italic").newline()
+                .fgBlue().underline().a("Blue and underline")
+        )
     }
 
     @Test
@@ -76,7 +95,7 @@ class CodeHighlighterTest {
             withContext(Dispatchers.Default) {
                 val lexer = Lexer(lang)
                 val decorations = lexer.decorate(content)
-                CodeHighlighter.highlight(content, decorations, AnsiHighlightBuilder)
+                CodeHighlighter.highlight(content, decorations, CodeHighlighterTest.AnsiHighlightBuilder)
             }
         }
         println(result)
--- codeview/src/test/resources/source/Test.kt
+++ codeview/src/test/resources/source/Test.kt
@@ -35,5 +35,7 @@ class Test {
         TODO("todo")
     }
 
+    /* End */
+
 
 }
